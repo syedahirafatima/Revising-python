95c81dee3caf3a4af0cb5f8e305d15f7e47f65c1
Syeda Hira Fatima <218037790+syedahirafatima@users.noreply.github.com>
2026-06-18 22:13:32 +0500

    Update operator_modulus.py

--- operator_modulus.py
+++ operator_modulus.py
@@ -3,3 +3,10 @@ a = eval(input(f"Enter \"a\" number:"))
 b = eval(input(f"Enter \"b\" number:"))
 remainder = a % b
 print(f"Remainder when {a} is divided by {b} is {remainder}")
+
+# Program for checking if the number is even or odd
+num  = 4
+if num % 2 == 0:
+    print(f"{num} is even.")
+else:
+    print(f"{num} is odd.")
