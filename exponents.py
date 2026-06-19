c78cfe92ca9317069451647c060c0bfe3a72dee8
Syeda Hira Fatima <218037790+syedahirafatima@users.noreply.github.com>
2026-06-19 20:18:24 +0500

    Update exponents.py

--- exponents.py
+++ exponents.py
@@ -3,3 +3,7 @@
 side = 4
 area = side**2 # as the formula is length^2
 print(f"The area of the square having equal sides of the meassurement {side} units is {area} square units. ")
+# Program for finding area of a square using user input
+side = eval(input("Enter the measurement of the side of the square: "))
+area = side**2 # as the formula is length^2
+print(f"The area of the square having equal sides of the meassurement {side} units is {area} square units. ")
