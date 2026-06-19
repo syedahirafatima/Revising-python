0875bbd7b98025fafec94ee845fd744f469f3f7c
Syeda Hira Fatima <218037790+syedahirafatima@users.noreply.github.com>
2026-06-19 20:16:30 +0500

    Update exponents.py

--- exponents.py
+++ exponents.py
@@ -1,1 +1,5 @@
 
+# Program for finding area of a square
+side = 4
+area = side**2 # as the formula is length^2
+print(f"The area of the square having equal sides of the meassurement {side} units is {area} square units. ")
