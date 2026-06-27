b5e5a0fe5cf9ad1ec39602b03cd6b9457d1a74ba
Syeda Hira Fatima <218037790+syedahirafatima@users.noreply.github.com>
2026-06-27 12:06:51 +0500

    Create comparison_operator(<).py

---
+++ comparison_operator(<).py
@@ -0,0 +1,14 @@
+#< for numerical data
+age = int(input("What is your age: "))
+if age < 18:
+    print("You are a minor.")
+# only practicing <
+
+
+# < for strings
+# it works in alphabetical order then (liek in dictionary)
+
+print("ac" < "ab")
+#false as ab (1st) comes before and ac comes after. 
+print("ab"< "ac")
+# true
